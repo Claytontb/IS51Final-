7880ab39411ba6d1b8a411d4f7412a463fa3c846
Claytontb <78582298+Claytontb@users.noreply.github.com>
2021-05-13 17:01:25 -0700

    Add files via upload

---
+++ Structured English IS51 Final.py
@@ -0,0 +1,22 @@
+
+"""
+The program is trying to translate a sentence captured as user input.
+We first read in the text file Final.txt.
+then from the text file, we create a list of strings from each lines in the text file.
+Then, we crere a dictionary the list.
+Once the text file has been read into memory, we close the file.
+
+open(Final.txt)
+input: numbers  
+intiate a iterator (counter) and accumulator and set it to zero 
+loop through the list test scores 
+add all the test scores 
+increment the computer by one 
+once the loop ends there are no more words to add 
+divide the accumulator (sum) by the counter
+display the output to the user 
+output: print the average of the class scores 
+
+
+
+"""
